f3999d2ac5cef7b8e50ae88a4a47166225bbc329
David Strauch <dmaster18@gmail.com>
2019-06-12 16:01:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 let musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-let instruments = ["Guitar", "Bass Guiatar", "Drums", "Keyboard"];
+let instruments = ["Guitar", "Bass Guiatar", "Lead Guitar", "Keyboard"];
 let musiciansAndInstruments = [];
 function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i <= musicians.length; i++)
