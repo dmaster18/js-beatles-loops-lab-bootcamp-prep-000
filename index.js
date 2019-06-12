3ce3156f826044e2b2797554a6902aa7a18aacd7
David Strauch <dmaster18@gmail.com>
2019-06-12 15:55:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 let instruments = ["Guitar", "Bass", "Drums", "Keyboard"];
 function theBeatlesPlay(musicians, instruments) {
-  for (let i = 0; i < musicians; i++)
+  for (let i = 0; i; i++)
 }
