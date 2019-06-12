4bcbeaa6c1e9d1e07c4bfde2dc9b03a7c5f21711
David Strauch <dmaster18@gmail.com>
2019-06-12 15:58:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ let musiciansAndInstruments = [];
 function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i <= musicians.length; i++)
   {
-    return musicians[i]
+    return musicians[i] + "plays " + 
   }
 }
